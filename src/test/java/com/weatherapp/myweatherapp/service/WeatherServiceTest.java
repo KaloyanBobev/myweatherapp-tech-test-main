552b377911faa0a03ae400631c891a794a89dacc
Kaloyan <kaloyanbobev@gmail.com>
2025-02-10 22:07:40 +0000

    Update WeatherServiceTest.java

--- src/test/java/com/weatherapp/myweatherapp/service/WeatherServiceTest.java
+++ src/test/java/com/weatherapp/myweatherapp/service/WeatherServiceTest.java
@@ -11,7 +11,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.time.LocalTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.when;
 
 public class WeatherServiceTest {
